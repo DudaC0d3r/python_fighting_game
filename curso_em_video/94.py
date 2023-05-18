0ecdab9ca78d2eadcb94df533ab4fd533d27a297
DudaC0d3r <dudakirara@gmail.com>
2023-05-17 21:42:32 -0300

    Create 94.py

---
+++ curso_em_video/94.py
@@ -0,0 +1,9 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('-' * tam)
+    print(msg)
+    print('-' * tam)
+    
+escreva("Gustavo Guanabara")
+escreva('curso de python no youtube')
+escreva("ceV")
