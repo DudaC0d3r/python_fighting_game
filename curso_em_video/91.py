6e08ea0b85390a6f0d64fe9faadc61c4a652e35a
DudaC0d3r <dudakirara@gmail.com>
2023-05-22 16:57:12 -0300

    Update 91.py

--- curso_em_video/91.py
+++ curso_em_video/91.py
@@ -1,9 +1,9 @@
 jogador = dict()
 partidas = list()
 jogador['nome'] = str(input("nome do jogador: "))
-tot = int(input(f"Quantas partidas {jogador['nome']} jogou? "))
+tot = int(input(f"Quantas partidas {jogador['nome']} jogou?: "))
 for c in range(0, tot):
-    partidas.append(int(input(f"      Quantos gols na partida {c}")))
+    partidas.append(int(input(f"      Quantos gols na partida {c}: ")))
 jogador['gols'] = partidas[:]
 jogador['total'] = sum(partidas)
 print("-=" * 30)
