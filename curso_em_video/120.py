5f4e2ab4ba0070042bd1237d52afdad6b045cedd
DudaC0d3r <dudakirara@gmail.com>
2023-07-06 21:53:22 -0300

    Update 120.py

--- curso_em_video/120.py
+++ curso_em_video/120.py
@@ -7,11 +7,24 @@ def leiaint():
                      print("erro! float tente novamente")
                 if l + 1 == len(i):
                     
-                    print("entrou")
+                    print("o numero inteiro digtado foi {}".format(i))
                     return False
         else:
              print("erro! str tente novamente")
+             
+             
 def leiafloat():
     while True:
-        f = str(input())
+        f = float(input("digite um real: "))
+        if f.isnumeric():
+            for q in range(len(f)):
+                if f[q] == ".":
+                    print("erro! float tente novamente")
+                if q + 1 == len(f):
+                    
+                    print("o numero real digitado foi {f}")
+                    return False
+        else:
+            print("erro! float tente novamente")
+
 leiaint()
