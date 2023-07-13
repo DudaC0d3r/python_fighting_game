d79114c45792ac122e816e3cf1bd6298688ba7f5
DudaC0d3r <dudakirara@gmail.com>
2023-07-13 16:37:01 -0300

    Update 122.py

--- curso_em_video/122.py
+++ curso_em_video/122.py
@@ -4,15 +4,17 @@ def menu_principal():
     print('----------MENU PRINCIPAL----------')
     print('(1) Cadastrar novo cliente')
     print('(2) Consultar cliente')
-    print('(3) Sair')
+    print('(3) Editar cliente')
+    print('(4) Excluir cliente')
+    print('(0) Sair')
 menu_principal()
 
 first_choise = int(input('Escolha a função desejada: '))
-while first_choise < 0 or first_choise > 3:
+while first_choise < 0 or first_choise > 5:
     print('Opção inválida. Por favor, selecione uma das opções do menu.')
     first_choise = int(input('Escolha a função desejada: '))
 else:
-    if first_choise == 3:
+    if first_choise == 0:
         print("Programa desenvolvido por @roddyzera. Obrigado por utilizar.")
         import time, sys
         for i in range(0, 10):
@@ -43,6 +45,9 @@ else:
             
     elif first_choise == 2:
         print(lista_clientes)
-        
+
     elif first_choise == 3:
-        print("finalizado")
+        print('Consultar cliente.')
+
+    elif first_choise == 4:
+        print('Consultar cliente.')
